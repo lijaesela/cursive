32b08a8ba978e5d6fd292358227cdef9920e411e
lijaesela <janponalukasu@tutanota.com>
2020-08-02 09:34:04 -0500

    cd better

--- cursive_cd.sh
+++ cursive_cd.sh
@@ -1,7 +1,9 @@
-# source this file or copy this function
+# source this file or copy this function to your shell's rc file
+# change "$cursive_tmp" to match the directory in config.h
 
 cursive_cd ()
 {
+	$cursive_tmp="/tmp/cursive_cd"
 	cursive
-	cd `cat /tmp/cursive_cd`
+	[ -f $cursive_tmp ] && cd `cat $cursive_tmp` || echo "cursive tmp file not found, cannot cd."
 }
